fef8c95594d604cd9e387fb876d01650c4c4c0e7
Ken Keyes <kkeyes1@cnm.edu>
2018-03-03 11:30:26 -0700

    Set up personal website environment, index, rain, and style files

---
+++ js/rain.js
@@ -0,0 +1,5 @@
+window.onload = function () { alert("It's loaded!") };
+
+// let canvas = document.getElementById("canvas");
+// let c = canvas.getContext('2d');
+
---
+++ public_html/index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta http-equiv="refresh" content="5" >
+		<title>Who Is Ken</title>
+
+<!--		Bootstrap CSS-->
+		<link
+			rel="stylesheet"
+			href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+			integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+			crossorigin="anonymous">
+
+<!--		Custom CSS-->
+		<link
+			rel="stylesheet"
+			href="/css/style.css"
+			type="text/css">
+
+<!--		Bootstrap JS Dependencies-->
+		<script
+			src="https://code.jquery.com/jquery-3.3.1.min.js"
+			integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+			crossorigin="anonymous"></script>
+		<script
+			src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+			integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+			crossorigin="anonymous"></script>
+		<script
+			src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+			integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
+			crossorigin="anonymous"></script>
+
+<!--		Custom JS-->
+		<script
+		src="/js/rain.js"
+		type="text/javascript">
+		</script>
+	</head>
+	<body>
+		<div class="jumbotron bg-dark rounded-0 h-100" id="canvas">
+		</div>
+	</body>
+</html>
